7090576540721c47f5c90c2e5156e14ddbdc9568
Christopher Fore <contact@chrisfore.us>
2023-05-14 08:27:06 -0400

    Add arg passthrough

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import csv
+import os
+import sys
+import subprocess
 
 def main():
+    args = sys.argv
+    del(args[0])
+
+    if len(args) > 0 and args[0] == '-h':
+        subprocess.run(['yt-dlp', '-h'])
+        exit(0)
+
+    argstr = ' '.join(str(x) for x in args)
+
+
     with open('subscriptions.csv', newline='') as subs:
         reader = csv.reader(subs)
         for row in reader:
             if len(row) == 0:
                 continue
             print(f'Downloading {row[2]}')
-            print(f'yt-dlp https://youtube.com/channel/{row[0]}')
+            cmd = f'yt-dlp https://youtube.com/channel/{row[0]} {argstr}'
+            print(cmd)
 
 if __name__ == '__main__':
     main()
